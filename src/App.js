263777c6f0a178f525743818b4c8b318cb48a25d
pigmore <pigmore@gmail.com>
2024-07-29 13:31:36 +0800

    scale max min

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import {Timelinememo} from './components/timeline';
 
 function App() {
   const [count,setCount]=useState(0)
+  const [scale,setScale]=useState(10)
   return (
     <div className="App">
       <button
         onClick={()=>{
-          setCount(count + 1)
+          setCount(count => count + 1)
         }}
       >{count}+1</button>
+      <button
+        onClick={()=>{
+          setScale(scale =>Math.min(scale + 1,20))
+        }}
+      >scale:{scale}+1</button>
+      <button
+        onClick={()=>{
+          setScale(scale =>Math.max(scale - 1,1) )
+        }}
+      >scale:{scale}-1</button>
       <Timelinememo/>
     </div>
   );
