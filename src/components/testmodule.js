9a34e707bb7f00d8ecf371b517658e0925b5d75e
pigmore <pigmore@gmail.com>
2024-07-30 14:52:58 +0800

    no message

--- src/components/testmodule.js
+++ src/components/testmodule.js
@@ -5,6 +5,8 @@ import { dragGraph } from './dragGraph';
 export const Test = forwardRef((props,ref)=> {
   let mycount = 1
   const [count,setCount] = useState(0)
+  const [jsonInit,setJsonInit] = useState('json转换结果')
+  const [inputJson,setinputJson] = useState('')
   useEffect(() => {
     async function init() {
       if (typeof window !== "undefined" || window.initReady !== true) {
@@ -25,15 +27,74 @@ export const Test = forwardRef((props,ref)=> {
 
   }));
 
+  const handleInitJson = ()=>{
+    var json = []
+    console.log(inputJson)
+    // new dragGraph(
+    //   randomInt(0, 1200), //x
+    //   randomInt(0, 280), //y
+    //   randomInt(10, 40), //w
+    //   20,  //h
+    //   `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)} , 1) `,
+    //   canvas,
+    //   'rectangle'
+    // );
+    for (var item of inputJson.data) {
+      for (var variable in item) {
+        if (item.hasOwnProperty(variable)) {
+          if (variable == 'bg_musics'){
+            const bg_musics_item = item[variable][0]
+            json.push({
+              x:bg_musics_item.start_time,
+              w:bg_musics_item.end_time - bg_musics_item.start_time,
+              y:0
+            })
+          }
+          if (variable == 'elements'){
+            for (var elementsItem of item[variable]) {
+              json.push({
+                x:elementsItem.start_time,
+                w:elementsItem.end_time - elementsItem.start_time,
+                y:elementsItem.layer_number
+              }
+
+              )
+            }
+
+          }
+          // if (variable == 'objects'){
+          //   for (var objectsItem of item[variable]) {
+          //     json.push(
+          //       {
+          //         x:objectsItem.start_time,
+          //         w:objectsItem.end_time - objectsItem.start_time,
+          //         y:objectsItem.layer_number
+          //       }
+          //     )
+          //   }
+          // }
+        }
+      }
+    }
+    setJsonInit(JSON.stringify(json))
+  }
+
 
   return (
     <div>
-      <input type="text" placeHolder="input the json data here"/>
+      <input type="text" placeholder="input the json data here" value={inputJson}
+        onChange={
+          (e)=>{
+            setinputJson(JSON.parse(e.target.value))
+          }
+        }
+      />
       <button onClick={
         ()=>{
-          console.log(mycount)
+          handleInitJson()
         }
-      }>{count}{mycount}</button>
+      }>initJson</button>
+      <p>{jsonInit}</p>
     </div>
   );
 })
